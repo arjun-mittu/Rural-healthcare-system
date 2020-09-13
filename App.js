092938bb13ce36f1e0e44df347f00f999bcc36d3
Arjun Mittu <43925857+arjun-mittu@users.noreply.github.com>
2020-09-13 23:47:54 +0530

    boiler plate

---
+++ App.js
@@ -0,0 +1,14 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import IndexScreen from './src/screens/IndexScreen';
+
+const navigator = createStackNavigator({
+    Index: IndexScreen
+},{
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+          title: "Health Care"
+        }
+});
+
+export default createAppContainer(navigator);
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import {Text, View, StyleSheet, InputText, Button} from 'react-native';
+
+
+const IndexScreen = props => {
+    return(
+        <View>
+            <Text> Index Screen </Text>
+        </View>
+    )
+}
+
+export default IndexScreen;
