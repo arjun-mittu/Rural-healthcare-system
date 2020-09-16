1cd885e5e9c5725747a8226ecc7fab684ab6f996
Tanmay Agarwal <54680350+Tanmay3610@users.noreply.github.com>
2020-09-16 22:55:55 +0530

    Add files via upload

---
+++ src/Styles.js
@@ -0,0 +1,27 @@
+import {StyleSheet} from 'react-native';
+
+export default Style = StyleSheet.create({
+    heading : {
+        textAlign: 'center',
+        fontSize: 35,
+        color: 'green'
+    },
+
+    textInput : {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingHorizontal: 10
+    },
+
+    buttonStyle: {
+        marginHorizontal: 15,
+        padding: 10,
+        textAlign: 'center',
+        fontSize: 35,
+        borderWidth: 1,
+        borderRadius: 50,
+        backgroundColor: 'green'
+    }
+});
