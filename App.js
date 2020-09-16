df2023ba6e2c42ee843b4bd5e47308a4d1b18423
Tanmay Agarwal <54680350+Tanmay3610@users.noreply.github.com>
2020-09-16 23:07:15 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import LoginScreen from './src/screens/LoginScreen';
+import HomeScreen from './src/screens/HomeScreen'
 
 const navigator = createStackNavigator({
     Login: LoginScreen,
