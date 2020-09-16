d93c2d59e474bcc77aaac7c89cefd9a5fbfe6e7b
Tanmay Agarwal <54680350+Tanmay3610@users.noreply.github.com>
2020-09-16 22:56:21 +0530

    Add files via upload

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,13 +1,43 @@
 import React, {useState, useEffect} from 'react';
-import {Text, View, StyleSheet, InputText, Button} from 'react-native';
-
+import {Text, View, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import Style from '../Styles';
+import { AntDesign } from '@expo/vector-icons';
 
 const IndexScreen = props => {
     return(
         <View>
-            <Text> Index Screen </Text>
+            <Text style = {Style.heading}> Login </Text>
+
+            <AntDesign style = {{
+                textAlign: 'center',
+                marginTop: 10
+            }}
+            name="user" size={75} color="green" />
+
+            <TextInput
+                style = {Style.textInput}
+                autoCapitalize = "none"
+                    autoCorrect = {false}
+                    placeholder = "Username"/>
+
+            <TextInput
+                 style = {Style.textInput}
+                 autoCapitalize = "none"
+                 autoCorrect = {false}
+                 placeholder = "Password"/>
+
+            <TouchableOpacity>
+                <Text style = {Style.buttonStyle}> LogIn </Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text style = {{
+                    textAlign: 'center',
+                    margin: 10
+                }}>Not Registered? Signup</Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
 
 export default IndexScreen;
