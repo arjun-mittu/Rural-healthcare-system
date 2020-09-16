eb9a2d5798f74cb860aab5ea047b154300ef9097
Tanmay Agarwal <54680350+Tanmay3610@users.noreply.github.com>
2020-09-16 23:02:43 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import IndexScreen from './src/screens/IndexScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 const navigator = createStackNavigator({
-    Index: IndexScreen
+    Login: LoginScreen,
+    Home: HomeScreen
 },{
-    initialRouteName: 'Index',
+    initialRouteName: 'Login',
     defaultNavigationOptions: {
           title: "Health Care"
         }
