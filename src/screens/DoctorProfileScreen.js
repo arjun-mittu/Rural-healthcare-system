99b67387520fd7dd21224aff7e110f61247d99b6
Tanmay Agarwal <54680350+Tanmay3610@users.noreply.github.com>
2020-09-22 15:59:05 +0530

    Add files via upload

---
+++ src/screens/DoctorProfileScreen.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import {Text, View, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import Style from '../Styles';
+
+const DoctorProfileScreen = props => {
+    return(
+        <View style = {Style.background}>
+            <Text>Doctor Profile Screen</Text>
+        </View>
+    )
+}
+
+
+export default DoctorProfileScreen;
