f1ccc7e5019e03472764bf07ceda4910204d48ab
Tanmay Agarwal <54680350+Tanmay3610@users.noreply.github.com>
2020-09-22 09:27:48 +0530

    Add files via upload

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,29 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-import LoginScreen from './src/screens/LoginScreen';
+import React, {useState, useEffect} from 'react';
+import {Text, View, StyleSheet, TextInput, Picker, TouchableOpacity} from 'react-native';
+import Style from '../Styles';
 
-const navigator = createStackNavigator({
-    Login: LoginScreen,
-    Home: HomeScreen
-},{
-    initialRouteName: 'Login',
-    defaultNavigationOptions: {
-          title: "Health Care"
-        }
-});
+const HomeScreen = props => {
+    const[pickerValue, changePickerValue] = useState('');
+    return(
+        <View>
+            <Text style = {Style.heading}>Home Screen </Text>
+            <Text style = {Style.subHeading}> About </Text>
+            <TouchableOpacity onPress = {() => props.navigation.navigate('Login')}>
+                <Text style = {{...Style.buttonStyle, marginTop: 20}}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress = {() => props.navigation.navigate('Signup')}>
+                <Text style = {{...Style.buttonStyle, marginTop: 20}}>Signup</Text>
+            </TouchableOpacity>
 
-export default createAppContainer(navigator);
+            <Picker style = {{width: '100%'}}
+                selectedValue = {pickerValue}
+                onValueChange = {(itemValue, itemIndex) => changePickerValue(itemValue)}>
+                <Picker.item label = 'Login' value = 'login' />
+                <Picker.item label = 'Signup' value = 'signup' />
+            </Picker>
+        </View>
+    )
+}
+
+
+export default HomeScreen;
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import {Text, View, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import Style from '../Styles';
+
+const SignupScreen = props => {
+    return(
+        <View>
+            <Text>Signup Screen </Text>
+        </View>
+    )
+}
+
+
+export default SignupScreen;
