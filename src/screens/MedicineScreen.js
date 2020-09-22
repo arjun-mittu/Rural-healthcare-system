6ceef477eebac479e5e80f277d9ff3c5a39f8456
Tanmay Agarwal <54680350+Tanmay3610@users.noreply.github.com>
2020-09-22 15:59:20 +0530

    Add files via upload

---
+++ src/screens/MedicineScreen.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react';
+import {Text, View, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import Style from '../Styles';
+
+const MedicineScreen = props => {
+    return(
+        <View style = {Style.background}>
+            <Text>Medicine Screen</Text>
+        </View>
+    )
+}
+
+
+export default MedicineScreen;
