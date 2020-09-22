b134f29c523ae4d736e587304961eba25b64ca6a
Tanmay Agarwal <54680350+Tanmay3610@users.noreply.github.com>
2020-09-22 15:59:47 +0530

    Add files via upload

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,35 +4,41 @@ import Style from '../Styles';
 import { AntDesign } from '@expo/vector-icons';
 
 const LoginScreen = props => {
+    const [username, changeUsernamne] = useState('');
+    const [password, changePassword] = useState('');
+    
     return(
-        <View>
-            <Text style = {Style.heading}> Login </Text>
-
+        <View style = {{...Style.background, flex: 1, justifyContent: 'center'}}>
             <AntDesign style = {{
                 textAlign: 'center',
                 marginTop: 10
             }}
-            name="user" size={75} color="green" />
+            name="user" size={75} color='rgb(3, 184, 234)' />
 
             <TextInput
                 style = {Style.textInput}
                 autoCapitalize = "none"
-                    autoCorrect = {false}
-                    placeholder = "Username"/>
+                value = {username}
+                onChangeText = {newValue => changeUsernamne(newValue)}
+                autoCorrect = {false}
+                placeholder = "Username"/>
 
             <TextInput
                  style = {Style.textInput}
                  autoCapitalize = "none"
+                 value = {password}
+                 onChangeText = {newValue => changePassword(newValue)}
                  autoCorrect = {false}
                  placeholder = "Password"/>
 
             <TouchableOpacity>
                 <Text style = {Style.buttonStyle}> LogIn </Text>
             </TouchableOpacity>
-            <TouchableOpacity>
+            <TouchableOpacity onPress = {() => props.navigation.navigate('Signup')}>
                 <Text style = {{
                     textAlign: 'center',
-                    margin: 10
+                    margin: 10,
+                    color: 'rgb(165, 165, 166)'
                 }}>Not Registered? Signup</Text>
             </TouchableOpacity>
         </View>
