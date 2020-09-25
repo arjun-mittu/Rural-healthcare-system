4abd1d76460ada00c24dc421c732786ca7a105ea
Tanmay Agarwal <54680350+Tanmay3610@users.noreply.github.com>
2020-09-25 10:07:38 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -6,8 +6,9 @@ import HomeScreen from './src/screens/HomeScreen'
 import SignupScreen from './src/screens/SignupScreen'
 import DoctorProfileScreen from './src/screens/DoctorProfileScreen'
 import MedicineScreen from './src/screens/MedicineScreen'
-import {UserProvider} from './src/context/UserContext';
 import PatientProfile from './src/screens/PatientProfile';
+import {Provider as AuthProvider} from './src/context/AuthContext';
+import {setNavigator} from "./src/navigationRef";
 
 const navigator = createStackNavigator({
     Login: LoginScreen,
@@ -26,11 +27,12 @@ const navigator = createStackNavigator({
 const App = createAppContainer(navigator);
 
 export default () => {
-    return <UserProvider>
-        <App/>
-    </UserProvider>
-}
-
+    return <>
+        <AuthProvider>
+            <App ref ={(navigator) => {setNavigator(navigator)}}/>
+        </AuthProvider>
+        </>
+};
 //DoctorProfile- Shubham 
 //MedicineTable- Manav
 //Signup- Mittu
