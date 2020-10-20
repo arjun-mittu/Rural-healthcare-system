458b3f38858e92583d18044c4384e82511a1efde
Tanmay3610 <agarwaltanmay3610@gmail.com>
2020-10-20 22:13:06 +0530

    Added Signup index

---
+++ src/components/PickerComponent.js
@@ -0,0 +1,28 @@
+import {Picker, Text, View} from "react-native";
+import React from "react";
+
+const renderPicker = (item) =>{
+    const value = [];
+    for(let i = 0; i < item.length; i++){
+        const pickerValue = item[i]
+        value.push(<Picker.Item value = {pickerValue} key = {i} label = {item[i]} />)
+    }
+    return value;
+};
+
+const PickerComponent = props => {
+    return (
+        <View style = {{margin: 15}}>
+        <Text style = {{color: 'rgb(3, 184, 234)'}}>{props.title}</Text>
+        <Picker
+            selectedValue={props.value}
+            style={{ height: 50, width: 125, color: 'rgb(3, 184, 234)'}}
+            onValueChange={(itemValue, itemIndex) => props.setValue(itemValue)}
+        >
+            {renderPicker(props.item)}
+        </Picker>
+        </View>
+    )
+}
+
+export default PickerComponent;
